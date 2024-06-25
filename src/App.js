69a283ff18f9ac464534b27f9bc9b92fffab4698
KRjasoncho <166813835+KRjasoncho@users.noreply.github.com>
2024-06-25 21:55:36 +0900

    [common]Tmate 작업시작

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 
 import './App.css';
-import {useState} from "react";
-import axios from "axios";
 
 function App() {
-  const [hello, setHello] = useState(null);
-
-  const btn =()=>{
-    axios.get('http://localhost:8080/api/hello')
-      .then(response=>setHello(response.data))
-      .catch(error => console.log(error))
-  }
 
   return (
     <div className="App">
-<div onClick={btn}>백엔드 통신 확인 {hello}</div>
+
     </div>
   );
 }
