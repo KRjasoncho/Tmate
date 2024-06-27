047a05d55710f47db5fa403d13fec0ec1f832724
KRjasoncho <166813835+KRjasoncho@users.noreply.github.com>
2024-06-27 20:50:12 +0900

    [login] 회원가입 http 통신(프론트 앤드) 구현

--- src/Store/LoginStore.js
+++ src/Store/LoginStore.js
@@ -1,4 +1,5 @@
-import {makeAutoObservable} from "mobx";
+import {makeAutoObservable, runInAction} from "mobx";
+import axios from "axios";
 
 class LoginStore{
 
@@ -18,6 +19,17 @@ class LoginStore{
     const {name, value} = e.target;
     this.signUpForm[name]= value;
   }
+  signUpHandler =()=>{
+  axios.post('api/user/create', {...this.signUpForm})
+    .then(res=>
+    runInAction(()=>
+      console.log(res)
+    ))
+    .catch(err=>
+    runInAction(()=>console.log(err)))
+  }
 }
+
+
 
 export default LoginStore;
--- src/View/login/SignUp.js
+++ src/View/login/SignUp.js
@@ -3,30 +3,34 @@ import {Observer, useLocalObservable} from "mobx-react";
 import LoginStore from "../../Store/LoginStore";
 
 const SignUp = () => {
-  const loginStore = useLocalObservable(()=>new LoginStore);
+  const loginStore = useLocalObservable(() => new LoginStore);
 
   return (
     <div>
       <p>회원가입</p>
       <Observer>
-        {()=>(
-      <form>
-        <div>
-          <label htmlFor='name'>아이디</label>
-          <input type='text' id='id' value={loginStore.signUpForm.id} name='id'
-                  onChange={loginStore.inputHandler}/>
-        </div>
-        <div>
-          <label htmlFor='password'>비밀번호</label>
-          <input type='password' id='password' value={loginStore.signUpForm.password} name='password'
-                  onChange={loginStore.inputHandler} />
-        </div>
-        <div>
-          <label htmlFor='nickName'>닉네임</label>
-          <input type='text' id={'nickName'} value={loginStore.signUpForm.nickName} name='nickName'
-                  onChange={loginStore.inputHandler}/>
-        </div>
-      </form>
+        {() => (
+          <form>
+            <div>
+              <label htmlFor='name'>아이디</label>
+              <input type='text' id='id' value={loginStore.signUpForm.id} name='id'
+                     onChange={loginStore.inputHandler}
+                     required/>
+            </div>
+            <div>
+              <label htmlFor='password'>비밀번호</label>
+              <input type='password' id='password' value={loginStore.signUpForm.password} name='password'
+                     onChange={loginStore.inputHandler}
+                     required/>
+            </div>
+            <div>
+              <label htmlFor='nickName'>닉네임</label>
+              <input type='text' id={'nickName'} value={loginStore.signUpForm.nickName} name='nickName'
+                     onChange={loginStore.inputHandler}
+                     required/>
+            </div>
+            <button>회원가입하기</button>
+          </form>
         )}
       </Observer>
     </div>
